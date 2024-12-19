5b5fdbf4526e513a49151611811cb9ef52b23cc4
ridwaanhall <erbyl14@gmail.com>
2024-12-20 00:02:36 +0700

    add me.py with user information functions

---
+++ hello_ridwaanhall/me.py
@@ -0,0 +1,14 @@
+def username():
+    return "ridwaanhall"
+
+def nim():
+    return "5210411257"
+
+def name():
+    return "Ridwan Halim"
+
+def email():
+    return "ridwaanhall.dev@gmail.com"
+
+def college():
+    return f"{nim()}_{name()}"
